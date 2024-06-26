48ae8b27f50c71a197b639624aa1374022d18bab
sbla686 <sbla686@wgu.edu>
2024-06-26 07:49:39 -0700

    Committing part H. Created a CheckoutController.

---
+++ src/main/java/com/example/demo/controllers/CheckoutController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controllers;
+
+
+import com.example.demo.services.CheckoutService;
+import com.example.demo.services.Purchase;
+import com.example.demo.services.PurchaseResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    @Autowired
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+
+}
